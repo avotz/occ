778e787e81a3273ff34d11d8779843ca16a1eef5
Alonso Avotz <alonso@avotz.com>
2018-09-04 13:05:02 -0600

    se corrigio menu

--- wp-content/themes/occ/inc/wc.php
+++ wp-content/themes/occ/inc/wc.php
@@ -26,13 +26,40 @@ remove_action('woocommerce_single_product_summary', 'woocommerce_template_single
 //quitamos short description
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
 
-function occ_redirect_checkout_add_cart($url)
-{
-    $url = get_permalink(get_option('woocommerce_checkout_page_id'));
-    return $url;
-}
+// function occ_redirect_checkout_add_cart($url)
+// {
+//     $url = get_permalink(get_option('woocommerce_checkout_page_id'));
+//     return $url;
+// }
 
-add_filter('woocommerce_add_to_cart_redirect', 'occ_redirect_checkout_add_cart');
+// add_filter('woocommerce_add_to_cart_redirect', 'occ_redirect_checkout_add_cart');
+
+add_filter('gettext', 'change_woocommerce_return_to_shop_text', 20, 3);
+
+function change_woocommerce_return_to_shop_text($translated_text, $text, $domain)
+{
+
+    switch ($translated_text) {
+
+        case 'Return to shop':
+
+            $translated_text = __('Return to Home', 'woocommerce');
+            break;
+
+    }
+
+    return $translated_text;
+}
+/**
+ * Changes the redirect URL for the Return To Shop button in the cart.
+ *
+ * @return string
+ */
+function wc_empty_cart_redirect_url()
+{
+    return get_site_url();
+}
+add_filter('woocommerce_return_to_shop_redirect', 'wc_empty_cart_redirect_url');
 
 // Hook in
 add_filter('woocommerce_checkout_fields', 'custom_override_checkout_fields');
