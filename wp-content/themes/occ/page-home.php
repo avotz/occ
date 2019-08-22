ac4bc9771eb15bb47023708cc1a4c08a47c8e54b
Alonso Chavarria <alonso@avotz.com>
2019-08-22 09:30:31 -0600

    fix frase de banner

--- wp-content/themes/occ/page-home.php
+++ wp-content/themes/occ/page-home.php
@@ -30,7 +30,7 @@ get_header();
                     </div>
                     <div class="slide" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/banner3.jpg')"><h2 class="animated fadeIn">WE WILL TAKE CARE OF THE REST</h2></div>
                     <div class="slide" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/banner5.jpg')">
-                      <h2 class="animated fadeIn">Allow yourself to be part of our success</h2>
+                      <h2 class="animated fadeIn">Allow us to be part of your success</h2>
                     </div>
                      <div class="slide" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/banner2.jpg')">
                       <h2 class="animated fadeIn">TOP QUALITY MANAGEMENT</h2>
