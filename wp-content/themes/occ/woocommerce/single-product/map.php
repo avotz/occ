0b3b903da1c7425406755a0db3869374c027a311
Alonso Chavarria <alonso@avotz.com>
2019-09-13 12:03:05 -0600

    se agrego map section pero sin ponerlo

---
+++ wp-content/themes/occ/woocommerce/single-product/map.php
@@ -0,0 +1,28 @@
+
+<?php
+
+/**
+ * Custom Specs
+ *
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+global $product;
+?>
+<section class="property-specs">
+    <div class="property-specs-container">
+        <h2>Neighborhood</h2>
+    </div>
+    <hr>
+    <div class="property-specs-info-container">
+      
+        <iframe style="border: 0;" src="<?php echo rwmb_meta('rw_property_map') ?>" width="800" height="600" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+        
+    </div>
+
+   
+    
+</section>
